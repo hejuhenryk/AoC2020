d1faea33d62af72e056cd606119ca3d2213957ab
Marcin Sawczuk-Szymkiewicz <sawczuk.marcin@gmail.com>
2020-12-22 21:37:09 +0100

    day 4 part B

--- dist/src/day4.js
+++ dist/src/day4.js
@@ -3,7 +3,7 @@ fetch("../dist/src/input4.txt")
     .then((res) => res.text())
     .then((res) => res.split("\n"))
     .then((res) => readDocs(res))
-    .then((res) => console.log(countValidPass(res)));
+    .then((res) => console.log(countValidPass2(res)));
 const readDocs = (docs) => {
     const unSortedDocs = [];
     let doc = {};
@@ -29,16 +29,35 @@ const countValidPass2 = (unsortedDocs) => {
     const byrV = (v) => +v >= 1920 && +v <= 2002;
     const iyrV = (v) => +v >= 2010 && +v <= 2020;
     const eyrV = (v) => +v >= 2020 && +v <= 2030;
-    const hgtV = (v) => v.slice(-2, v.length) === "cm"
-        ? +v.slice(0, -2) >= 150 && +v.slice(0, -2) <= 193
-        : v.slice(-2, v.length) === "in"
-            ? +v.slice(0, -2) >= 59 && +v.slice(0, -2) <= 76
-            : false;
-    //   const hclV = (v: string) => +v >= 1920 && +v <= 2002  (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-    const eclV = (v) => v ===
-        ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].find((c) => c === v);
-    // const pidV = (v: string) => v   (Passport ID) - a nine-digit number, including leading zeroes.
-    return unsortedDocs.filter((d) => d.byr && d.iyr && d.eyr && d.hgt && d.hcl && d.ecl && d.pid).length;
+    const hgtV = (v) => {
+        v = v.trim();
+        const isTrue = v.slice(-2, v.length) === "cm"
+            ? +v.slice(0, -2) >= 150 && +v.slice(0, -2) <= 193
+            : v.slice(-2, v.length) === "in"
+                ? +v.slice(0, -2) >= 59 && +v.slice(0, -2) <= 76
+                : false;
+        return isTrue;
+    };
+    const hclV = (v) => /^#[0-9a-fA-F]{6}$/g.test(v.trim());
+    const eclV = (v) => ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(v.trim());
+    const pidV = (v) => /^\d{9}$/g.test(v.trim());
+    return unsortedDocs.filter((d) => {
+        if (d.pid && !pidV(d.pid))
+            console.log(d.pid, "-");
+        return (d.byr &&
+            d.iyr &&
+            d.eyr &&
+            d.hgt &&
+            d.hcl &&
+            d.ecl &&
+            d.pid &&
+            byrV(d.byr) &&
+            iyrV(d.iyr) &&
+            eyrV(d.eyr) &&
+            hgtV(d.hgt) &&
+            hclV(d.hcl) &&
+            eclV(d.ecl) &&
+            pidV(d.pid));
+    }).length;
 };
-2;
 //# sourceMappingURL=day4.js.map
