b7364abc9f4de0d0ba6fe4aa123f364a9b4a1524
Marcin Sawczuk-Szymkiewicz <sawczuk.marcin@gmail.com>
2020-12-23 20:38:51 +0100

    day 5 part A

--- dist/src/day5.js
+++ dist/src/day5.js
@@ -1,2 +1,14 @@
 "use strict";
+fetch("../dist/src/input5.txt")
+    .then((res) => res.text())
+    .then((res) => res.split("\n"))
+    .then((res) => res.map((code) => readCode(code)))
+    .then((res) => res.reduce((pre, cur) => (pre > cur ? pre : cur), 0))
+    .then((res) => console.log(res));
+const readCode = (code) => {
+    const row = code.slice(0, 7);
+    const colum = code.slice(-3);
+    return (parseInt(row.replace(/[B]/g, "1").replace(/[F]/g, "0"), 2) * 8 +
+        parseInt(colum.replace(/[L]/g, "0").replace(/[R]/g, "1"), 2));
+};
 //# sourceMappingURL=day5.js.map
--- src/day5.ts
+++ src/day5.ts
@@ -1,3 +1,15 @@
 fetch("../dist/src/input5.txt")
   .then((res) => res.text())
-  .then((res) => console.log(res.split("\n"))
+  .then((res) => res.split("\n"))
+  .then((res) => res.map((code) => readCode(code)))
+  .then((res) => res.reduce((pre, cur) => (pre > cur ? pre : cur), 0))
+  .then((res) => console.log(res));
+
+const readCode = (code: string) => {
+  const row = code.slice(0, 7);
+  const colum = code.slice(-3);
+  return (
+    parseInt(row.replace(/[B]/g, "1").replace(/[F]/g, "0"), 2) * 8 +
+    parseInt(colum.replace(/[L]/g, "0").replace(/[R]/g, "1"), 2)
+  );
+};
