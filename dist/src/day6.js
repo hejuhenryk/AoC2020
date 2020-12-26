3d9cfe5c45bc1872be47f5178d5f61a2c2e22226
Marcin Sawczuk-Szymkiewicz <sawczuk.marcin@gmail.com>
2020-12-26 23:12:44 +0100

    day 6 part B

--- dist/src/day6.js
+++ dist/src/day6.js
@@ -5,19 +5,41 @@ fetch("../dist/inputs/input6.txt")
     .then((res) => group(res))
     .then((res) => console.log(res.reduce((p, c) => c + p, 0)));
 const group = (answers) => {
-    let yes = {};
+    let yes = {
+        persInGroup: 0,
+    };
     const numYes = [];
     answers.forEach((line) => {
         line = line.trim();
         if (/^\s*$/.test(line)) {
-            numYes.push(Object.keys(yes).length);
-            yes = {};
+            let num = 0;
+            Object.keys(yes).forEach((k) => {
+                if (k !== "persInGroup") {
+                    if (yes[k] === yes.persInGroup)
+                        num++;
+                }
+            });
+            numYes.push(num);
+            yes = { persInGroup: 0 };
         }
         else if (line) {
-            line.split("").forEach((letter) => yes[letter]++);
+            yes.persInGroup++;
+            line.split("").forEach((letter) => {
+                if (yes[letter])
+                    yes[letter]++;
+                else
+                    yes[letter] = 1;
+            });
         }
     });
-    numYes.push(Object.keys(yes).length);
+    let num = 0;
+    Object.keys(yes).forEach((k) => {
+        if (k !== "persInGroup") {
+            if (yes[k] === yes.persInGroup)
+                num++;
+        }
+    });
+    numYes.push(num);
     return numYes;
 };
 //# sourceMappingURL=day6.js.map
